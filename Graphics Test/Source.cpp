dc58caba2fb5f156dfff1b959218f2ca722c9475
Karimmunir1 <60698802+Karimmunir1@users.noreply.github.com>
2020-10-12 08:52:20 +0200

    Rotation Test

--- Graphics Test/Source.cpp
+++ Graphics Test/Source.cpp
@@ -3,7 +3,12 @@
 
 using namespace std;
 
+void drawTriangle();
+void drawAxes();
+
 int main() {
+	float r = 0.1f;
+
 	cout << "Koko" << endl;
 	if (!glfwInit()) {
 		cout << "Failed to init glfw" << endl;
@@ -26,26 +31,62 @@ int main() {
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//glPushMatrix();
-		glRotatef(1.5f, 0.0f, 0.0f, 1.0f);
+		drawAxes();
 
-		glBegin(GL_TRIANGLES);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glPushMatrix();
+		//glTranslatef(-0.5f, -0.5f, 1.0f);
+		glRotatef((450 * (3.1415 / 180)) , 0.0f, 0.0f, 1.0f);
+		//glTranslatef(0.5f, 0.5f, 1.0f);
+		drawTriangle();
+		glPopMatrix();
 
-		//glPopMatrix();
+		r += 0.1;
+
+		glPushMatrix();
+		glScalef(1.0f, -1.0f, 0.0f);
+		drawTriangle();
+		glPopMatrix();
+
+		glPushMatrix();
+		glScalef(-1.0f, -1.0f, 0.0f);
+		drawTriangle();
+		glPopMatrix();
+
+		glPushMatrix();
+		glScalef(-1.0f, 1.0f, 0.0f);
+		drawTriangle();
+		glPopMatrix();
 
 		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+void drawTriangle() {
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(0.25f, 0.25f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.5f, 0.75f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0.75f, 0.25f);
+	glEnd();
+}
+
+void drawAxes() {
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex2f(0.0f, 1.0f);
+	glVertex2f(0.0f, -1.0f);
+	glEnd();
+
+	glBegin(GL_LINES);
+	glVertex2f(1.0f, 0.0f);
+	glVertex2f(-1.0f, 0.0f);
+	glEnd();
 }
